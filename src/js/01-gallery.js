187a376f171b66587b37304cff74e541f5a9590a
Oleh Baramykin <olegix7891@ukr.net>
2022-12-07 18:47:32 +0200

    дз1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import cardTemp from '../template/cards.hbs';
+const galaryDiv = document.querySelector('.gallery');
+
+const createGalery = collection =>
+  galaryDiv.insertAdjacentHTML('afterbegin', collection.map(cardTemp).join(''));
+
+createGalery(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+const onImgClick = event => {
+  event.preventDefault();
+};
+
+galaryDiv.addEventListener('click', onImgClick);
 // Change code below this line
 
 console.log(galleryItems);
