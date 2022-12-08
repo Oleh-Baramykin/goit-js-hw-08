dc23b0e39940f564b0897e8dc356aaccba53da04
Oleh Baramykin <olegix7891@ukr.net>
2022-12-08 10:19:01 +0200

    дз3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+const throttle = require('lodash.throttle');
+const form = document.querySelector('.feedback-form');
+const LOCAL_DATA_NAME = 'feedback-form-state';
+const currentLocal = JSON.parse(localStorage.getItem(LOCAL_DATA_NAME));
 
+const localPrint = {};
+
+const onFormInput = event => {
+  localPrint[event.target.name] = event.target.value;
+  localStorage.setItem(LOCAL_DATA_NAME, JSON.stringify(localPrint));
+};
+form.addEventListener('input', throttle(onFormInput, 500));
+
+const fillForm = () => {
+  currentLocal && currentLocal.email
+    ? (form.email.value = currentLocal.email)
+    : null;
+  currentLocal && currentLocal.message
+    ? (form.message.value = currentLocal.message)
+    : null;
+};
+fillForm();
+
+const onSubmitClick = event => {
+  event.preventDefault();
+  console.log({ email: form.email.value, message: form.message.value });
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCAL_DATA_NAME);
+};
+form.addEventListener('submit', onSubmitClick);
